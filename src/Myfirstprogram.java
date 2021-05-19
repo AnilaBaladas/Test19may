bb1a919d25b4a63eabc7e1edbb37e9fdebf251ff
AnilaBaladas <anilabaladas@gmail.com>
2021-05-19 01:44:58 -0400

    i changed second time

--- src/Myfirstprogram.java
+++ src/Myfirstprogram.java
@@ -4,7 +4,7 @@ public class Myfirstprogram {
 
     public static void main(String[] args) {
 	String s1="Hello world";
-	String s2="new girl";
+	String s2="new girl changedg";
         System.out.println(s1+s2);
     }
 }
